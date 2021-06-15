91fa5af239fc408b891a77d31e3e30881506ef63
averoli <64157643+averoli@users.noreply.github.com>
2021-06-15 13:03:16 +0200

    Function for ActiveSlide

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const slides = document.querySelectorAll('.slide');
+function SlidesPlugin(activeSlide = 1) {
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-        slide.classList.add('active')
-    })
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active')
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses () {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+SlidesPlugin();
