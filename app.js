9bda9e9340a91f7ae57a15ad9e6a62598ea76be0
averoli <64157643+averoli@users.noreply.github.com>
2021-06-20 18:44:02 +0200

    Aim Game

--- app.js
+++ app.js
@@ -1,38 +1,77 @@
-const upBtn = document.querySelector('.up-button');
-const downBtn = document.querySelector('.down-button');
+const startBtn = document.querySelector('#start');
+const screens = document.querySelectorAll('.screen');
+const timeList = document.querySelector('#time-list');
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('.board')
 
-const sidebar = document.querySelector('.sidebar');
-const mainSlide = document.querySelector('.main-slide');
+let time = 0
+let score = 0
 
-const slidesCount = mainSlide.querySelectorAll('div').length;
-const container = document.querySelector('.container');
-
-sidebar.style.top = `-${(slidesCount - 1) * 100}vh`;
-
-let activeSlideIndex = 0;
-
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    screens[0].classList.add('up')
 })
 
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
-})
-
-function changeSlide(direction) {
-    if(direction === 'up') {
-        activeSlideIndex++
-        if (activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0;
-        }
-    } else if (direction === 'down') {
-            activeSlideIndex--
-            if (activeSlideIndex < 0) {
-                activeSlideIndex = slidesCount - 1
-            }
+timeList.addEventListener('click', event => {
+    if (event.target.classList.contains('time-btn')) {
+        time = parseInt(event.target.getAttribute('data-time'))
+        screens[1].classList.add('up')
     }
-    const height = container.clientHeight
+    startGame();
+})
 
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+board.addEventListener('click', event => {
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
+
+function startGame() {
+    setInterval(decreaseTime, 1000)
+    createRandomCircle()
+    setTime(time)
 }
+
+function decreaseTime() {
+    if (time === 0) {
+        finishGame()
+    } else {
+        let current = --time;
+        if (current < 10) {
+            current = `0${current}`
+        }
+        setTime(current)
+    }
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`
+}
+
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Score: <span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect();
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${x}px`
+    circle.style.left = `${y}px`
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
